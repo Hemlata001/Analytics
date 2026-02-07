a1fa033b41546e7ad3958bd785de45d346b7373a
Hemlata <127768311+Hemlata001@users.noreply.github.com>
2026-02-07 15:26:02 +0530

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,10 @@
+# dictionary
+d = {10:100,20:200,30:300,40:400}
+print(d)
+print(type(d))  # type
+d[10] = 600 # updating
+print(d)
+d[50] = 500 # creating
+print(d)
+del d[30] # deleting
+print(d)
